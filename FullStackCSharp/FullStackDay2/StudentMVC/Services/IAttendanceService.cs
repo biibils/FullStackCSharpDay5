3f6bce860d12a212b6089e32239308dc92636676
biibils <billy.sutawijaya.04@gmail.com>
2026-01-14 20:54:14 +0700

    delete some folder unrelated

--- FullStackCSharp/FullStackDay2/StudentMVC/Services/IAttendanceService.cs
+++
@@ -1,9 +0,0 @@
-using StudentMVC.Models;
-
-namespace StudentMVC.Services;
-
-public interface IAttendanceService
-{
-	void CreateAttendance(Attendance attendance);
-	IEnumerable<Attendance> GetByStudent(int studentId);
-}
